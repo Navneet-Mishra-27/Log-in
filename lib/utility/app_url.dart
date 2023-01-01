2ced83d41ad43a77bd79a7ebe2216d579c80f842
Navneet Mishra <navneetmishra027112001@gmail.com>
2023-01-02 01:13:08 +0530

    first commit

---
+++ lib/utility/app_url.dart
@@ -0,0 +1,7 @@
+class AppUrl {
+  static const String baseUrl = 'http://192.168.1.225:3006/api/users/v1/login';
+
+  static const String login = baseUrl + '/ApiLogin';
+  static const String register = baseUrl + '/registration';
+  static const String forgotPassword = baseUrl + '/forgot_password';
+}
